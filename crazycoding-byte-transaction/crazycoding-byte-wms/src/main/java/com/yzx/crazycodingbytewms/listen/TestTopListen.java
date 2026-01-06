67008dbab9db22a956f7211b446f70f75b02b7c2
yzx18 <944166810@qq.com>
2026-01-07 04:54:05 +0800

    feat:玩玩flux

--- crazycoding-byte-transaction/crazycoding-byte-wms/src/main/java/com/yzx/crazycodingbytewms/listen/TestTopListen.java
+++ crazycoding-byte-transaction/crazycoding-byte-wms/src/main/java/com/yzx/crazycodingbytewms/listen/TestTopListen.java
@@ -16,20 +16,14 @@ import org.springframework.stereotype.Component;
 @Component
 // 5.x 消费者注解（核心：maxReconsumeTimes=0 触发死信）
 @RocketMQMessageListener(
-        topic = "test_top", // 监听的业务主题
-        consumerGroup = "test-consumer-group", // 与yml中一致
-        consumeMode = ConsumeMode.CONCURRENTLY, // 并发消费
-        messageModel = MessageModel.CLUSTERING, // 集群模式（默认）
-        maxReconsumeTimes = 0 // 优先级最高：重试次数为0，失败/超时直接进死信
+        topic = "delay_topic", // 监听的业务主题
+        consumerGroup = "test-consumer-group"// 与yml中一致
 )
 public class TestTopListen implements RocketMQListener<String> {
 
     @Override
     public void onMessage(String s) {
-        // 模拟「未消费/消费失败」场景：
-        // 场景1：故意抛异常 → 消费失败，无重试直接进死信
-        // 场景2：不处理 → 消息超时未ACK，触发死信
-        System.out.println("接收到消息：" + s);
-        throw new RuntimeException("模拟消费失败，触发死信队列");
+        System.out.println("接受时间"+System.currentTimeMillis());
+        System.out.println("接收到延迟消息：" + s);
     }
 }
